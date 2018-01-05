c765e3db98a18760d5f1d63e9fef56660cd4ef28
JaeWoongHwang <jwngh15@gmail.com>
2018-01-05 06:28:27 +0000

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery-migrate-min
+//= require jquery_ujs
+//= require turbolinks
+//= require popper
+//= require bootstrap
+
+//= require hs.megamenu
+//= require widget
+//= require widgets/menu
+//= require widgets/mouse
+//= require widgets/slider
+//= require chosen.jquery
+//= require slick
+//= require jquery.fancybox.min
+//= require hs.core.js
+//= require components/hs.header
+//= require helpers/hs.hamburgers
+//= require components/hs.dropdown
+//= require components/hs.slider
+//= require components/hs.select
+//= require components/hs.carousel
+//= require components/hs.popup
+//= require components/hs.go-to
